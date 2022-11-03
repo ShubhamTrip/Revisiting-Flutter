cd1e4846c02636d00df768023bda63569b71ed60
Shubham Tripathi <shubhamtrip16@gmail.com>
2022-11-03 22:21:03 +0530

    first app added

---
+++ first/lib/main.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Home());
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
