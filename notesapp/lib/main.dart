252f63ee0f4dce7703dab7bbba11ea92ddaebd21
Shubham Tripathi <shubhamtrip16@gmail.com>
2022-11-05 19:06:10 +0530

    supa

--- notesapp/lib/main.dart
+++ notesapp/lib/main.dart
@@ -1,6 +1,15 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
-void main() {
+import 'package:flutter/material.dart';
+import "package:supabase_flutter/supabase_flutter.dart";
+
+const supabaseUrl = 'https://bcdqrcwvtqbeknkafree.supabase.co';
+const supabaseKey = String.fromEnvironment(
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjZHFyY3d2dHFiZWtua2FmcmVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc2NDg1NzgsImV4cCI6MTk4MzIyNDU3OH0.2DVyfsmG5GBvyDqor3lr5vdbvtNG7XAqiPe56NtayfY');
+
+Future<void> main() async {
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
+  // ignore: prefer_const_constructors
   runApp(const NotesApp());
 }
 
@@ -17,11 +26,38 @@ class _NotesAppState extends State<NotesApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          toolbarHeight: 0,
-        ),
-      ),
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            toolbarHeight: 50,
+            title: Text(
+              'Notes App',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          body: ListView(
+            children: [
+              Container(
+                height: 200,
+                width: double.infinity,
+                color: Colors.amber,
+              ),
+              Container(
+                height: 200,
+                width: double.infinity,
+                color: Colors.blue,
+              ),
+              Container(
+                height: 200,
+                width: double.infinity,
+                color: Colors.black,
+              ),
+              Container(
+                height: 200,
+                width: double.infinity,
+                color: Colors.red,
+              )
+            ],
+          )),
     );
   }
 }
