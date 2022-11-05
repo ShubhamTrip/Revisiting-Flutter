dc5bad61d349ca5247d33ee9afc7318abd7336e9
Shubham Tripathi <shubhamtrip16@gmail.com>
2022-11-05 18:07:34 +0530

    notesapp created

--- first/lib/main.dart
+++ first/lib/main.dart
@@ -6,9 +6,14 @@ void main() {
   runApp(Home());
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,6 +23,12 @@ class Home extends StatelessWidget {
           toolbarHeight: 0,
           backgroundColor: Colors.black,
         ),
+        body: SafeArea(
+            child: Container(
+          height: 100,
+          width: 100,
+          color: Colors.amber,
+        )),
       ),
     );
   }
---
+++ notesapp/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatefulWidget {
+  const NotesApp({super.key});
+
+  @override
+  State<NotesApp> createState() => _NotesAppState();
+}
+
+class _NotesAppState extends State<NotesApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          toolbarHeight: 0,
+        ),
+      ),
+    );
+  }
+}
