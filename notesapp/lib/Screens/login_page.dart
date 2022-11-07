94fea5f92bdf57cf07f086481d1ffa3c288a6167
Shubham Tripathi <shubhamtrip16@gmail.com>
2022-11-07 23:00:20 +0530

    adding google login

--- notesapp/lib/Screens/login_page.dart
+++ notesapp/lib/Screens/login_page.dart
@@ -101,19 +101,32 @@ class LoginPage extends StatelessWidget {
                     SizedBox(
                       height: 30,
                     ),
-                    GestureDetector(
-                      child: Container(
-                          child: ListTile(
-                        leading: Text('New Here? Register.'),
-                        // trailing: Container(child: Image.asset(''),),
-                        isThreeLine: false,
-                      )),
-                      onTap: () => {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => SignUpPage()))
-                      },
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        GestureDetector(
+                          child: Container(
+                              child: Text(
+                            'New Here? Register.',
+                            style: TextStyle(color: Colors.blue),
+                          )),
+                          onTap: () => {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SignUpPage()))
+                          },
+                        ),
+                        GestureDetector(
+                          child: Container(
+                              child: Image.asset(
+                            'assets/google.png',
+                            height: 30,
+                            width: 30,
+                          )),
+                          onTap: () => {},
+                        )
+                      ],
                     )
                   ],
                 ))));
